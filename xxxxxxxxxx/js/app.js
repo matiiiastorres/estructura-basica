ba7408f62734e9d7b3fc37f601b5088c9a99c0aa
wall3n <franmoreno1506@gmail.com>
2022-04-26 21:06:28 +0200

    fix: arreglado problemas de composicion de funciones y asincronia

--- xxxxxxxxxx/js/app.js
+++ xxxxxxxxxx/js/app.js
@@ -5,21 +5,14 @@ const getData = async () => {
     const resp = await fetch(" https://pokeapi.co/api/v2/pokemon")
     const json = await resp.json()
 
-
     return (json.results)
 
 }
 
-getData()
-
 const createData = async () => {
     const infoApiCovalenthq = await getData()
     const containerCovalent = document.getElementById("containerCovalent")
-     
-   const pokemones = infoApiCovalenthq.map(pokemon => "<p>${pokemon.name}</p>")
+   const pokemones = infoApiCovalenthq.map(pokemon => `<p>${pokemon.name}</p>`)
 
-containerCovalent.innerHTML = pokemones
-
-
-
+    return containerCovalent.innerHTML = pokemones
 }
